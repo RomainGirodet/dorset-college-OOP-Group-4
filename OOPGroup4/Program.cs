c1062c370d7c9f15c13b5efd0231c3a816174a89
RomainGirodet <romain.girodet@edu.devinci.fr>
2020-10-24 22:58:54 +0200

    Add all member's name

--- OOPGroup4/Program.cs
+++ OOPGroup4/Program.cs
@@ -2,11 +2,19 @@
 
 namespace OOPGroup4
 {
+
+    // Corentin Chatin 23201
+    // Alexis Gibert 22841
+    // Pablo Acevedo Rincon 23169
+    //Louis Bader 23197
+    // Alexis Cerio 22829
+    // Romain Girodet 23437
     class Program
     {
         static void Main(string[] args)
         {
            //test_Push-Romain_Girodet
+
                 
         }
     }
