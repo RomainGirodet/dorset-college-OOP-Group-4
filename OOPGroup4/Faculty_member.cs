35f5c4f972ec79661e39098a63ffeafc1ac1d661
Corentin Chatin <23201@student.dorset-college.ie>
2020-10-25 13:33:48 +0000

    add structure of functions of facultyMember Class

--- OOPGroup4/Faculty_member.cs
+++ OOPGroup4/Faculty_member.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace OOPGroup4
 {
+    // Corentin Chatin 23201
+
     class Faculty_member
     {
+
+        public List<Class> liste_Classe { get; set; }
+        public List<Student> my_student { get; set; }
+
+        public Faculty_member()
+        {
+
+        }
+        public void Assesment(Class classeA)
+        {
+
+        }
+        public void Grade(Student george)
+        {
+
+        }
+        public void Acces_Info()
+        {
+
+        }
+        public void Repport_Missing(Student mickael)
+        {
+
+        }
+
+
+
     }
 }
+
