b5f53033bf7d9442fabb67f4426102e8a86182b8
Corentin Chatin <23201@student.dorset-college.ie>
2020-10-31 15:20:51 +0000

    add VerifyLoging (bool) and info about private or public variable

--- OOPGroup4/Member_of_school.cs
+++ OOPGroup4/Member_of_school.cs
@@ -6,18 +6,36 @@ namespace OOPGroup4
 {
     class Member_of_school : School
     {
-        string id;
-        string name;
-        string surname;
-        string password;
-        int age;
-        string sexe;
-        string mail;
+       
+        private string id;
+        public string name {get;set;}
+        public string surname {get;set;}
+        private string password ;
+        public int age {get;set;}
+        public string sexe {get;set;}
+        private string email ;
+        
+
 
         public  Member_of_school(string place, string school_name, int number_of_student,string id,string name, string surname,string password, 
-            int age, string sexe, string mail ) : base( place,  name, number_of_student)
+            int age, string sexe, string email ) : base( place,  name, number_of_student)
         {
+            this.id =id ;
+            this.name =name ;
+            this.surname = surname ;
+            this.password =password ;
+            this.age =age;
+            this.sexe =sexe;
+            this.email =email ;
+        }
+        public bool VerifyLogin(string idinsert , string passwordInsert){
+        
+                 if (passwordInsert == password & idinsert==id){
+                 return true ;
+                 }
+                else {return false;}
 
         }
+
     }
 }
