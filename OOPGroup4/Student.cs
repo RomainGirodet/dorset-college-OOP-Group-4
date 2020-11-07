72edda6c07083f7fafbec100dce0f68749a48298
LouisB23197 <23197@student.dorset-college.ie>
2020-11-07 15:56:37 +0100

    classe student

--- OOPGroup4/Student.cs
+++ OOPGroup4/Student.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace OOPGroup4
 {
-    class Student
+    class Student : Member_of_school
     {
+        private List<Payment> payment_list;
+        private List<Absence> absence_list;
+        public Student(string place, string school_name, int number_of_student, string id, string name, string surname, string password, int age, string sexe, string mail) : base(place, name, number_of_student, id, name, surname, password, age, sexe, mail)
+        {
+
+        }
+        void Pay(string payer_name,string payment_method,double amount)
+        {
+
+        }
     }
 }
