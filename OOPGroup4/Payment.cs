4b41b9da40f432ccae8ac25ec751add9598e3759
LouisB23197 <23197@student.dorset-college.ie>
2020-11-07 16:30:17 +0100

    class payment

--- OOPGroup4/Payment.cs
+++ OOPGroup4/Payment.cs
@@ -6,6 +6,15 @@ namespace OOPGroup4
 {
     class Payment
     {
-
+        private string payer_name;
+        private string payment_method;
+        private double amount;
+        private DateTime date;
+        public Payment(string payer_name, string payment_method, double amount)
+        {
+            this.payer_name = payer_name;
+            this.payment_method = payment_method;
+            this.amount = amount;
+        }
     }
 }
