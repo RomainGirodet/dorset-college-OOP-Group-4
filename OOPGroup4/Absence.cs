06dfac9b8a7701d6420f43cd105d3a49de846ed5
Alexis Cerio <alexis111@orange.fr>
2020-11-10 14:17:30 +0000

    Active = Booléen

--- OOPGroup4/Absence.cs
+++ OOPGroup4/Absence.cs
@@ -7,23 +7,20 @@ namespace OOPGroup4
     class Absence
     {
         // Test branche push
-        //test 2.1
-        //coucou alexis :)
-        //vive Napoléon Bonaparte 
-        // All-in Doge
+
         // Champs :
 
         public string classes; // Name of classes.
         public int date; // Date of absence.
         public int timeslot; // Time slot of absence.
         public int time; // Duration of absence in hours.
-        public int active = 1; // allows to know if the absence is effective or not (1 if it is, 0 if not).
+        public bool active = true; // allows to know if the absence is effective or not (true if it is, false if not).
         public string comments; // Optional
 
 
         // Constructeur :
 
-        public Absence(string classes, int date, int timeslot, int time) // Don't need active because it's 1 by default, nether comments because it's optional.
+        public Absence(string classes, int date, int timeslot, int time) // Don't need active because it's true by default, nether comments because it's optional.
         {
             this.classes = classes;
             this.date = date;
@@ -86,7 +83,7 @@ namespace OOPGroup4
             }
         }
 
-        public int Active
+        public bool Active
         {
             get
             {
--- OOPGroup4/Admin.cs
+++ OOPGroup4/Admin.cs
@@ -15,150 +15,21 @@ namespace OOPGroup4
 
         }
 
-      
-        #region Subject_fonction
-        // Créer une nouvellle matière
-        public void NewSubject(List<Course> listSubject, string name)
-        {
 
-        }
-        //Changer le nom d'une matière
-        public void ChangeSubjectName(List<Course> listSubject, string newName, string OldName)
-        {
-
-        }
-       
-        #endregion
-        #region TimeTable_function
-        /// <summary>
-        /// Create a new timeTable.
-        /// </summary>
-        /// <param name="listClasse"></param>
-        /// <param name="nomClasse"></param>
-        /// <param name="listSubject"></param>
-        public void CreateTimeTable()
-        {
-
-        }
-        /// <summary>
-        /// Add new subject to the admin's TimeTable wich the student will be able to choose 
-        /// for their own TimeTable.
-        /// </summary>
-        /// <param name="listClasse"></param>
-        /// <param name="nomClasse"></param>
-        /// <param name="listSubject"></param>
-        public void Add_SubjectToTimetable(string studentID)
-        {
-
-        }
-        
-        public void Delete_SubjectFromTimetable(string studentID)
-        {
-
-        }
-        //public void Exchange_SubjectFromTimetable(string studentID, List<Course> listSubject)
-        //{
-
-        //}
-        public void Move_SubjectFromTimetable(string studentID)
-        {
-
-        }
-        public void Modify_SubjectFromTimetable(string studentID)
-        {
-
-        }
-        #endregion
-        #region Payement_and_Invoice_Function
-        public void CreateInvoice(string studentID)
-        {
-
-        }
-        public void MofifyInvoiceDeadline(string studentID)
-        {
-
-        }
-        public void MofifyInvoiceAmount(string studentID)
-        {
-
-        }
-        public void MofifyInvoiceName(string studentID)
-        {
-
-        }
-
-        #endregion
         #region Absence_fonction
         /// <summary>
         /// Allows to change the parameter "active" from the absence from 1 to 0.
         /// The admin can then add a comment to justificate the absence.
         /// </summary>
         /// <param name="nameStudent"></param>
-        public void JustifyAnAbsence(string studentID)
+        public void JustifyAnAbsence(int studentID, int absNumber, string comment)
         {
-
+            student[studentID].absence[absNumber].active = 0;
+            student[studentID].absence[absNumber].comments = comment;
         }
         #endregion
         #region Get_information_fonction
-
-        /// <summary>
-        /// Get the name, field of study, name of the class supervised, 
-        /// nb of hours worked per week and the information relative 
-        /// to the classe Member_of_school
-        /// </summary>
-        public void GetTeacherFile(string teacherID)
-        {
-
-        }
-        /// <summary>
-        /// Get  the information relative to the classe Member_of_school
-        /// in addition of the name of the class, the number of absences and 
-        /// a boolean saying if the student has payed everithing.
-        /// </summary>
-        public void GetStudentFile(string studentID)
-        {
-
-        }
-        /// <summary>
-        /// Get the historic of the bills payed, their date and amount.
-        /// 
-        /// </summary>
-        public void GetStudentBillHistoric(string studentID)
-        {
-
-        }
-        /// <summary>
-        /// Get the historic of the bills payed linked to a specific invoice with their date and amount.
-        /// </summary>
-        public void GetStudentBillHistoricOfOneInvoice(string studentID)
-        {
-
-        }
-        /// <summary>
-        /// Get all the invoice of the student with the amount to pay, the amount already payed and the deadline.
-        /// </summary>
-        public void GetStudentInvoice(string studentID)
-        {
-
-        }
-        /// <summary>
-        /// Get all the grade of the student sorted by domain with the average in each domain 
-        /// Get also the average grade of the student.
-        /// </summary>
-        public void GetStudentGrade(string studentID)
-        {
-
-        }
-        /// <summary>
-        /// Get the historical of the student's absences with their date 
-        /// and a comment on their justification.
-        /// </summary>
-        public void GetStudentAbsenceHistorical(string studentID)
-        {
-
-        }
         #endregion
-        
 
     }
 }
